86ad789f4dc474fd2525b994daa6fc269b2f9d0c
Tyler Ramage <tysamigo@gmail.com>
2020-07-22 21:31:04 -0400

    remove https redirection

--- api/Startup.cs
+++ api/Startup.cs
@@ -36,8 +36,6 @@ namespace api
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseHttpsRedirection();
-
             app.UseRouting();
 
             app.UseAuthorization();
